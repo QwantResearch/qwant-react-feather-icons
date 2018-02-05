a0c31badeb3d3a2f08395be75a0f9cfc93e38325
g.lariviere <g.lariviere@qwant.com>
2018-02-05 17:27:05 +0100

    [ADD] name props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
-import { createElement } from 'react'
 import PropTypes from 'prop-types'
 
-const Icon = ({ name }) => createElement(require(`./icons/${name}`))
+import All from './icons/all'
+import Chevron from './icons/chevron'
+import Cross from './icons/cross'
+import Images from './icons/images'
+import List from './icons/list'
+import Logo from './icons/logo'
+import Music from './icons/music'
+import News from './icons/news'
+import Search from './icons/search'
+import Settings from './icons/settings'
+import Shopping from './icons/shopping'
+import Social from './icons/social'
+import Videos from './icons/videos'
+import Web from './icons/web'
+
+const Icon = ({ name }) => {
+  switch (name) {
+    case 'all': return All
+    case 'chevron': return Chevron
+    case 'cross': return Cross
+    case 'images': return Images
+    case 'list': return List
+    case 'logo': return Logo
+    case 'music': return Music
+    case 'news': return News
+    case 'search': return Search
+    case 'settings': return Settings
+    case 'shopping': return Shopping
+    case 'social': return Social
+    case 'videos': return Videos
+    case 'web': return Web
+    default: return false
+  }
+}
 
 Icon.propTypes = {
   name: PropTypes.oneOf([
